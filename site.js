75d4c8ab15f0602bdcde2181baed37eece4d56dc
Nagisa7Shiota <121880460+Nagisa7Shiota@users.noreply.github.com>
2023-01-23 18:02:19 +0100

    Test

---
+++ site.js
@@ -0,0 +1,5 @@
+GetWH()
+{
+    document.getElementById("mainhtml").height = System.Windows.Forms.SystemInformation.PrimaryMonitorSize.height;
+    document.getElementById("mainhtml").width = System.Windows.Forms.SystemInformation.PrimaryMonitorSize.width;
+}
