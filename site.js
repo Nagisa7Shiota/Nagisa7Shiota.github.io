f0efa49cdb44b0c2b1b4d2b9899ab81ffa62589d
Nagisa7Shiota <121880460+Nagisa7Shiota@users.noreply.github.com>
2023-01-23 19:25:05 +0100

    Wrappertxt and Wrappertitle
    Both now fixed for mobile!

--- site.js
+++
@@ -1,5 +0,0 @@
-GetWH()
-{
-    document.getElementById("mainhtml").height = System.Windows.Forms.SystemInformation.PrimaryMonitorSize.height;
-    document.getElementById("mainhtml").width = System.Windows.Forms.SystemInformation.PrimaryMonitorSize.width;
-}
